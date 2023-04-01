dea0af14044aa597b353d5e910a8826e3392c6ce
Manmohan Rajpal <91263531+Manmohan-Rajpal@users.noreply.github.com>
2023-04-01 14:06:22 -0400

    Add files via upload

---
+++ Q7/input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a=0;
+int b=0;
+//add a+b
+int sum=a+b;
+//print sum
+}
---
+++ Q7/output.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a=0;
+int b=0;
+
+int sum=a+b;
+
+}
